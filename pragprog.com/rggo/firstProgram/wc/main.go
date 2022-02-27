f412308c2bf71a4f23260da5f6aaf70ab25e0633
Vishnu Ganesan <vishnukg@gmail.com>
2022-02-27 21:05:32 +1100

    Added tests

--- pragprog.com/rggo/firstProgram/wc/main.go
+++ pragprog.com/rggo/firstProgram/wc/main.go
@@ -2,18 +2,50 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io"
-	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+
+	array := [5]int{10, 20, 30, 40, 50}
+
+	for _, v := range array {
+		println(v)
+	}
+
+	var array1 [3]*string
+	array2 := [3]*string{new(string), new(string), new(string)}
+
+	*array2[0] = "Red"
+	*array2[1] = "Blue"
+	*array2[2] = "Green"
+
+	array1 = array2
+
+	for _, v := range array1 {
+		println(*v)
+	}
+
+	slice := []int{10, 20, 30, 40, 50}
+
+	newSlice := slice[1:3]
+
+	println(len(newSlice), len(slice))
+
+	// lines := flag.Bool("l", false, "Count lines")
+	// flag.Parse()
+
+	// fmt.Println(count(os.Stdin, *lines))
+
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
+
 	wc := 0
 
 	for scanner.Scan() {
