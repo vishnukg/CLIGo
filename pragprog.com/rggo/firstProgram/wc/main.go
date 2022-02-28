adaa262cdd1bdf54d2d727b99a48d5bff94ba3a4
Vishnu Ganesan <vishnukg@gmail.com>
2022-02-28 13:08:53 +1100

    Removed practice code

--- pragprog.com/rggo/firstProgram/wc/main.go
+++ pragprog.com/rggo/firstProgram/wc/main.go
@@ -2,40 +2,18 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
 	"io"
+	"os"
 )
 
 func main() {
 
-	array := [5]int{10, 20, 30, 40, 50}
+	lines := flag.Bool("l", false, "Count lines")
+	flag.Parse()
 
-	for _, v := range array {
-		println(v)
-	}
-
-	var array1 [3]*string
-	array2 := [3]*string{new(string), new(string), new(string)}
-
-	*array2[0] = "Red"
-	*array2[1] = "Blue"
-	*array2[2] = "Green"
-
-	array1 = array2
-
-	for _, v := range array1 {
-		println(*v)
-	}
-
-	slice := []int{10, 20, 30, 40, 50}
-
-	newSlice := slice[1:3]
-
-	println(len(newSlice), len(slice))
-
-	// lines := flag.Bool("l", false, "Count lines")
-	// flag.Parse()
-
-	// fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines))
 
 }
 
