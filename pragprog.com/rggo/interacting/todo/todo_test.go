ac14e003a0775bb9207d5be68a73fd1d014b2bd5
Vishnu Ganesan <vishnukg@gmail.com>
2022-03-01 21:39:34 +1100

    Add test file

---
+++ pragprog.com/rggo/interacting/todo/todo_test.go
@@ -0,0 +1,40 @@
+package todo_test
+
+import (
+	"testing"
+
+	"pragprog.com/rggo/interacting/todo"
+)
+
+func TestAdd(t *testing.T) {
+	l := todo.List{}
+	taskName := "New Task"
+
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead", taskName, l[0].Task)
+	}
+}
+
+func TestComplete(t *testing.T) {
+	l := todo.List{}
+	taskName := "New Task"
+
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead", taskName, l[0].Task)
+	}
+
+	if l[0].Done {
+		t.Errorf("New task should not be completed")
+	}
+
+	l.Complete(1)
+
+	if !l[0].Done {
+		t.Errorf("New task shoud be completed")
+	}
+
+}
